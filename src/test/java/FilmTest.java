ce722901381cefddb11b92c72e32f6d44aa6dd6c
Haseeb <haseeb091097@gmail.com>
2022-12-22 11:24:51 +0000

    fixed failed filmtest

--- src/test/java/FilmTest.java
+++ src/test/java/FilmTest.java
@@ -18,7 +18,7 @@ public class FilmTest {
 
         Assertions.assertEquals(1,film.getFilmid(),"film id should be 1 but isnt");
         Assertions.assertEquals(2,film.getLanguageId(),"language id should be 2 but isnt");
-        Assertions.assertEquals(2012,film.getReleaseYear().toString(),"release year should be 2012 ");
+        Assertions.assertEquals("2012",film.getReleaseYear().toString(),"release year should be 2012 ");
         Assertions.assertEquals("this is movie description",film.getDescription(),"movie description not correct ");
         Assertions.assertEquals("iron man",film.getTitle(),"movie title not correct ");
 
@@ -28,5 +28,5 @@ public class FilmTest {
     }
 
 
-   
+
 }
