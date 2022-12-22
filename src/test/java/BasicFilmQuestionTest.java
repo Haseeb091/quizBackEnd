a26352e712d16149529179f4bfcf097cce0f51f4
Haseeb <haseeb091097@gmail.com>
2022-12-22 16:55:10 +0000

    added more test for question maker in filmquestion

--- src/test/java/BasicFilmQuestionTest.java
+++ src/test/java/BasicFilmQuestionTest.java
@@ -23,6 +23,57 @@ public class BasicFilmQuestionTest {
         Assertions.assertEquals("2020",basicFilmQuestion.getCorrectAnswer(),"the correct answer value is not right");
 
 
+
+        incorrectAnswers=new ArrayList<>();
+        incorrectAnswers.add("english");
+        incorrectAnswers.add("french");
+         basicFilmQuestion= new BasicFilmQuestion("Academy Dinasaur","language",incorrectAnswers,"japanese",1);
+        Assertions.assertEquals("what language was the movie Academy Dinasaur created in?",basicFilmQuestion.getQuestion(),"question is not correct");
+        Assertions.assertEquals("language",basicFilmQuestion.getQuestionType(),"question type is not language is not correct");
+        Assertions.assertEquals(3,basicFilmQuestion.getPossibleAnswers().size(),"the list size should be 3 including the correct answers");
+        Assertions.assertEquals("japanese",basicFilmQuestion.getCorrectAnswer(),"the correct answer value is not right");
+
+
+    }
+
+    @Test
+    public void BasicFilmQuestionConstructorQuestionTypeLanguage(){
+
+        ArrayList<String>incorrectAnswers=new ArrayList<>();
+        incorrectAnswers.add("english");
+        incorrectAnswers.add("french");
+        BasicFilmQuestion basicFilmQuestion= new BasicFilmQuestion("Academy Dinasaur","language",incorrectAnswers,"japanese",1);
+        Assertions.assertEquals("what language was the movie Academy Dinasaur created in?",basicFilmQuestion.getQuestion(),"question is not correct");
+        Assertions.assertEquals("language",basicFilmQuestion.getQuestionType(),"question type is not language is not correct");
+
+
+    }
+
+    @Test
+    public void BasicFilmQuestionConstructorQuestionTypeActor(){
+
+        ArrayList<String>incorrectAnswers=new ArrayList<>();
+        incorrectAnswers.add("movie1");
+        incorrectAnswers.add("movie2");
+        BasicFilmQuestion basicFilmQuestion= new BasicFilmQuestion("bob","actor",incorrectAnswers,"movie3",1);
+        Assertions.assertEquals("what movie was  bob in?",basicFilmQuestion.getQuestion(),"question is not correct");
+        Assertions.assertEquals("actor",basicFilmQuestion.getQuestionType(),"question type is not actor is not correct");
+
+
+
+    }
+
+    @Test
+    public void BasicFilmQuestionConstructorQuestionTypeCat(){
+
+        ArrayList<String>incorrectAnswers=new ArrayList<>();
+
+        BasicFilmQuestion basicFilmQuestion= new BasicFilmQuestion("Action","category",incorrectAnswers,"movie3",1);
+        Assertions.assertEquals("what movie is apart of  Action category?",basicFilmQuestion.getQuestion(),"question is not correct");
+        Assertions.assertEquals("category",basicFilmQuestion.getQuestionType(),"question type is not category is not correct");
+
+
+
     }
 
 
